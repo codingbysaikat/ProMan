345e94e5be1350d6b60aecd0e96b767cfc0ab017
saikat mondal <62213201+codingbysaikat@users.noreply.github.com>
2022-07-25 01:28:50 +0600

    changes

---
+++ js/main.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function () {
+    
+	jQuery('header nav').meanmenu(
+        {
+            meanMenuContainer: 'header',
+            meanScreenWidth: "880",
+            
+
+
+        }
+    );
+    
+
+});
